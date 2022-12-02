6b087f7f7f5442ecdd2c397a92ed301940dd6783
Vinicius Martins dos Santos <111471147+viniciusmsca@users.noreply.github.com>
2022-12-02 15:05:11 -0500

    Initial project

---
+++ src/main/java/com/savecoins/savecoinsapi/SaveCoinsApiApplication.java
@@ -0,0 +1,14 @@
+package com.savecoins.savecoinsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveCoinsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveCoinsApiApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/savecoins/savecoinsapi/SaveCoinsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savecoins.savecoinsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavecoinsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
